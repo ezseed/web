2fa6f5a99ab58993106a62bad945719f80042ff0
soyuka <soyuka@gmail.com>
2014-09-22 08:36:19 +0200

    chore(aurora): Init

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,52 @@
 var gulp = require('gulp')
   , concat = require('gulp-concat')
-  , bower = require('gulp-bower-files')
+  , bower = require('main-bower-files')
   , uglify = require('gulp-uglify')
   , ngmin = require('gulp-ngmin')
   , rename = require('gulp-rename')
-  , sass = require('gulp-sass')
+  , filter = require('gulp-filter')
   , add = require('gulp-add-src')
+  , sass = require('gulp-sass')
   , minifyCSS = require('gulp-minify-css')
+  , autoprefixer = require('gulp-autoprefixer')
+  , replace = require('gulp-replace')
+  , p = require('path')
 
 var paths = {
   scripts: ['./src/js/**/**/*.js', './src/js/**/*.js', './src/js/*.js'],
   styles: ['./src/scss/*.scss', './src/scss/**/*.scss']
 }
+
 /**
  * Get bower src, concats them together
  */
 gulp.task('bower', function() {
-  bower()
+  return gulp.src(
+    bower({
+      filter: function(path) {
+        return p.extname(path) == '.js'
+      }
+    })
+  )
     .pipe(add('bower_components/modernizr/modernizr.js'))
     .pipe(concat('vendor.js'))
     // .pipe(uglify())
     .pipe(gulp.dest('./dist/js'))
 })
 
-gulp.task('scripts', function() {
+gulp.task('aurora:copy', function() {
+  return gulp.src('./src/aurora/*.js.map')
+    .pipe(gulp.dest('./dist/js'))
+})
+
+gulp.task('aurora', ['aurora:copy'], function() {
+  return gulp.src('./src/aurora/*.js') 
+    .pipe(concat('aurora.js'))
+    // .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+})
+
+gulp.task('scripts', ['aurora'], function() {
   gulp.src(paths.scripts)
     .pipe(ngmin())
     .on('error', function(err) {
@@ -37,8 +60,24 @@ gulp.task('scripts', function() {
 })
 
 gulp.task('styles', function() {
-  gulp.src(paths.styles)
+  return gulp.src(paths.styles)
   .pipe(sass({errLogToConsole: true, includePaths: ['./bower_components']}))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions', 'last 2 Chrome versions'],
+    cascade: false
+  }))
+  // .pipe(add( 
+  //   bower({
+  //     filter: function(path) {
+  //       return p.extname(path) == '.css'
+  //     }
+  //   })
+  // ))
+  .pipe(concat('app.css'))
+  // .pipe(replace('font\/vjs', function() {
+  //
+  //   return '../../font/vjs'
+  // }))
   .pipe(gulp.dest('./dist/css'))
   .pipe(minifyCSS())
   .pipe(rename('app.min.css'))
@@ -46,8 +85,9 @@ gulp.task('styles', function() {
 
 })
 
-gulp.task('default', ['bower', 'scripts', 'styles'])
+gulp.task('default', ['bower', 'aurora', 'scripts', 'styles'])
 gulp.task('watch', function() {
+  gulp.watch(paths.aurora, ['aurora'])
   gulp.watch(paths.scripts, ['scripts'])
   gulp.watch(paths.styles, ['styles'])
 })
---
+++ src/aurora/get.sh
@@ -0,0 +1,2 @@
+wget --no-check-certificate http://audiocogs.org/codecs/js/flac.js ./$1.js
+

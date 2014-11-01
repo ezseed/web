1294e35582e80387b5a3e811965d49d37aa671de
soyuka <soyuka@gmail.com>
2014-11-01 23:34:40 +0100

    fix(tvShowPacker): Fix series ordering, clear left at % 6 rows

--- src/js/filters/tvShowsPacker.js
+++ src/js/filters/tvShowsPacker.js
@@ -12,14 +12,14 @@ angular.module('ezseed')
 
     var shows = []
 
-    //Transform movies in a key => value array
+    //Transform movies in a key => value array containing every season of the same serie
     for(var i in movies) {
       var e = movies[i]
 
       if(!shows[e.name]) {
         shows[e.name] = []
       } 
-      
+
       shows[e.name].push(e)
     }
 
@@ -38,19 +38,37 @@ angular.module('ezseed')
       blocks.push({width: l, shows: shows[o], name: o})
     }
 
-    //sort the blocks according to the width - better results with the Packer
+    //sort the blocks according to the width
     blocks.sort(function(a, b) {
       return a.width - b.width
     })
 
+    var numCols = 6
+    var numBlocks = blocks.length
+    var u = 0
+    var currentPosition = 0, nextBreak = numCols 
+
     // styling length for a 6 row items
-    for(var u in blocks) {
+    for(u; u < numBlocks; u++) {
       //count num covers until u
       blocks[u].until = u == 0 ? blocks[u].shows.length : blocks[u-1].until + blocks[u].shows.length
       //count num of the first cover of the show
       blocks[u].num = u == 0 ? 0 : blocks[u].until - blocks[u].shows.length 
+
+      currentPosition += blocks[u].width
+
+      //take a break
+      if(blocks[u].num >= nextBreak) {
+        nextBreak += numCols
+      }
+
+      blocks[u].style = {}
+      //if nb cols left + num > 6
+      // or
       //style = %6 ==0 (new row) clear
-      blocks[u].style = blocks[u].num % 6 == 0 ? {clear: 'left'} : {}
+      if(blocks[u].width + blocks[u].num > nextBreak || blocks[u].num % numCols == 0) {
+        blocks[u].style = {clear: 'left'}
+      }
     }
 
     return blocks

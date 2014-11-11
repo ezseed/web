48bd6c2379d2da0b5f2f1f08c1db23826e917514
soyuka <soyuka@gmail.com>
2014-11-11 14:23:00 +0100

    fix(divx): Direct link with location origin

--- src/js/controllers/items/movie.js
+++ src/js/controllers/items/movie.js
@@ -32,6 +32,7 @@ angular.module('ezseed')
     video.download = "/movies/"+video._id+"/download" //used with vlc
     video.stream = "/movies/"+video._id+"/stream/" + md.os() || md.userAgent()
     video.direct_link = window.location.origin + video.download
+    video.direct_stream = location.origin + '/download?read=true&path='+video.path
 
     $scope.video = video
     //if the player has been initiated before call dispose
@@ -42,8 +43,9 @@ angular.module('ezseed')
     }
 
     if(isPluginInstalled('divx')) {
+      console.log(video)
       $log.debug('DIVX Installed')
-      video_container.innerHTML =  "<embed id='DIVX' type='video/divx' pluginspage='http://go.divx.com/plugin/download/' width='720px' height='480px' style='display: inline-block;' autoplay='yes' src='"+video.download+"'></embed>" 
+      video_container.innerHTML =  "<embed id='DIVX' type='video/divx' pluginspage='http://go.divx.com/plugin/download/' width='720px' height='480px' style='display: inline-block;' src='"+video.direct_stream+"'></embed>"
     } else if(isPluginInstalled('vlc')) {
       $log.debug('VLC installed')
       video_container.innerHTML =  "<embed id='VLC' type='application/x-vlc-plugin' pluginspage='http://www.videolan.org' width='720px' height='480px' style='display: inline-block;' autoplay='yes' target='"+video.download+"'></embed>" 

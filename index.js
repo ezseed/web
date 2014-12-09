d9b2485a6d363ea3c5411a0b02a999e5c0076710
soyuka <soyuka@gmail.com>
2014-12-09 22:06:22 +0100

    feat(): Adds a template engine, ezseed now gives config and a router element

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 var express = require('express')
+var fs = require('fs')
 
-module.exports = function(app) {
+module.exports = function(router, app, config) {
   app.use(express.static(__dirname, {maxAge: 3600}))
 
-  app.get('/', function(req, res){
-    res.render('index.html')
+  app.engine('ntl', function (filePath, options, callback) { // define the template engine
+    fs.readFile(filePath, function (err, content) {
+      if (err) throw new Error(err);
+      // this is an exteremly simple template engine
+      var rendered = content.toString().replace('#base#', options.base)
+      return callback(null, rendered)
+    })
   })
 
-  app.get('/locales.json', function(req, res) {
+  app.set('view engine', 'ntl') // register the template engine
+
+  app.set('views', __dirname) // specify the views directory
+
+  //main view
+  router.get('/', function(req, res){
+    res.render('index', {base: config.base})
+  })
+
+  router.get('/locales.json', function(req, res) {
     res.json(require('./locales/locale-'+req.query.lang))
   })
+
 }
